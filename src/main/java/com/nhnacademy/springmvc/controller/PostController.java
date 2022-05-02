e4f7cc591a4fe1612d5206e82dea36daaf4a6f94
cororing <corock.dev@gmail.com>
2022-05-02 16:30:42 +0900

    예외처리 전까지

--- src/main/java/com/nhnacademy/springmvc/controller/PostController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/PostController.java
@@ -1,30 +1,39 @@
 package com.nhnacademy.springmvc.controller;
 
+import com.nhnacademy.springmvc.domain.Post;
 import com.nhnacademy.springmvc.repository.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/post")
+@RequestMapping("/post/{postId}")
 public class PostController {
     private final PostRepository postRepository;
+
+    @ModelAttribute("post")
+    public Post getPost(@PathVariable("postId") long postId) {
+        return postRepository.getPost(postId);
+    }
 
     public PostController(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
 
     // TODO #2: 게시물 조회 구현
-    @GetMapping("/{postId}")
-    public String viewPost() {
-        // ...
+    @GetMapping
+    public String viewPost(@ModelAttribute("post") Post post, Model model) {
+        model.addAttribute("post", post);
         return "postView";
     }
 
     // TODO #3: 게시물 수정 form 구현
-    @GetMapping("/{postId}/modify")
-    public String postModifyForm() {
-        // ...
+    @GetMapping("/modify")
+    public String postModifyForm(@ModelAttribute("post") Post post, Model model) {
+        model.addAttribute("post", post);
         return "postModify";
     }
 }
--- src/main/java/com/nhnacademy/springmvc/controller/PostRegisterController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/PostRegisterController.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.springmvc.controller;
 
+import com.nhnacademy.springmvc.domain.Post;
 import com.nhnacademy.springmvc.repository.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/post/register")
 public class PostRegisterController {
     private final PostRepository postRepository;
+
+    @ModelAttribute("post")
+    public Post getPost(@PathVariable("postId") long postId) {
+        return postRepository.getPost(postId);
+    }
 
     public PostRegisterController(PostRepository postRepository) {
         this.postRepository = postRepository;
@@ -23,8 +31,10 @@ public class PostRegisterController {
 
     // TODO #1: 게시물 등록 처리 구현
     @PostMapping
-    public ModelAndView registerPost() {
-        return null;
+    public ModelAndView registerPost(@ModelAttribute("post") Post post) {
+        ModelAndView mav = new ModelAndView("postRegister");
+        mav.addObject("post", post);
+        return mav;
     }
 
 }
