626792956aa8f409b43e264c7370e114660c1175
cororing <corock.dev@gmail.com>
2022-05-02 16:48:40 +0900

    주석 수정 (board2 브랜치 체크아웃 전)

--- src/main/java/com/nhnacademy/springmvc/exception/UserNotFoundException.java
+++ src/main/java/com/nhnacademy/springmvc/exception/UserNotFoundException.java
@@ -1,5 +1,5 @@
 package com.nhnacademy.springmvc.exception;
 
-// checked exception 은 메서드 시그니처에 반영된다
+// 왜 extends RuntimeException? checked exception 은 메서드 시그니처에 반영된다
 public class UserNotFoundException extends RuntimeException {
 }
