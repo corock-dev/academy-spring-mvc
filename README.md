bf8669274600c52d063a2d6697dd34f25d06f0ed
cororing <corock.dev@gmail.com>
2022-05-03 10:45:35 +0900

    실습을 해버렸네

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Spring MVC
 
+API Gateway     (공통처리: 인증)
+    |
+    |
+    |
+    /       /           /
+    주문      상품      결제
--- src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.springmvc.controller;
 
 import com.nhnacademy.springmvc.domain.User;
 import com.nhnacademy.springmvc.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,11 @@ public class UserRestController {
     }
 
     @GetMapping("/users/{userId}")
-    public User getUser(@PathVariable("userId") String userId) {
-        return userRepository.getUser(userId);
+    // public User getUser(@PathVariable("userId") String userId) {
+    public ResponseEntity<User> getUser(@PathVariable("userId") String userId) {
+        // ResponseEntity.notFound().build();
+        return ResponseEntity.ok()
+                             .header("X-123", "e43344")
+                             .body(userRepository.getUser(userId));
     }
 }
