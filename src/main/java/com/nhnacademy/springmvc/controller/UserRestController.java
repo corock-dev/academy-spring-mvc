c9276980e56682707e5cfc9d56e415dcf7eb2ad6
cororing <corock.dev@gmail.com>
2022-05-03 11:17:38 +0900

    11시 수업 전까지

--- src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
@@ -1,10 +1,15 @@
 package com.nhnacademy.springmvc.controller;
 
 import com.nhnacademy.springmvc.domain.User;
+import com.nhnacademy.springmvc.domain.UserRegisterRequest;
 import com.nhnacademy.springmvc.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +27,12 @@ public class UserRestController {
         return ResponseEntity.ok()
                              .header("X-123", "e43344")
                              .body(userRepository.getUser(userId));
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody UserRegisterRequest userRequest) {
+        userRepository.addUser(userRequest.getId(),
+            userRequest.getPassword(), userRequest.getAge(), userRequest.getName());
     }
 }
--- src/main/java/com/nhnacademy/springmvc/domain/UserRegisterRequest.java
+++ src/main/java/com/nhnacademy/springmvc/domain/UserRegisterRequest.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.springmvc.domain;
 
-import lombok.Value;
+import lombok.Data;
 
-@Value
+// [Refactor] - [Delombok] - [@Value]
+// @Value
+// @Data 기본 생성자랑 Setter 를 제공해준다.
+@Data
 public class UserRegisterRequest {
     String id;
     String password;
