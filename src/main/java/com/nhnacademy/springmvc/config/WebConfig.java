9da7a03b2555c6f8a6009d8aaf2dc662d7d7c6df
cororing <corock.dev@gmail.com>
2022-05-03 11:33:26 +0900

    registry 실습 전 설명 부분 커밋

--- src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.nhnacademy.springmvc.controller.ControllerBase;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Spring 에서 registry 라는 이름이 붙으면 저걸 통해서 뭘 등록할 떄 쓰는 양식같은 거라고 보면 된다.
+        registry.addViewController("/").setViewName("index");
+
+        // /version1/xxx    ->    /version2/aaa/xxx
+        // FIXME: 이게 OCP?
+        registry.addRedirectViewController("/ver1/xxx", "ver2/aaa/xxx");
     }
 }
--- src/main/java/com/nhnacademy/springmvc/controller/HomeController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/HomeController.java
@@ -3,11 +3,10 @@ package com.nhnacademy.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-}
+// @Controller
+// public class HomeController {
+//     @GetMapping("/")
+//     public String index() {
+//         return "index";
+//     }
+// }
