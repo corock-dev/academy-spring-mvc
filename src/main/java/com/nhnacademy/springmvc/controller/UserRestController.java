8e72b734bce8cb6f73b8b1bdbeb4b2fe0e892362
cororing <corock.dev@gmail.com>
2022-05-03 10:35:09 +0900

    UserRestController 추가

--- src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/UserRestController.java
@@ -1,22 +1,21 @@
 package com.nhnacademy.springmvc.controller;
 
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.nhnacademy.springmvc.domain.User;
+import com.nhnacademy.springmvc.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-// 몇 개는 REST API 랑 뭐랑 같이 나가야 하면 를 쓰고
-// @RestController == @Controller + @ResponseBody
-// @Controller
-// @ResponseBody
-
-// 전체 다(?) REST API 를 쓸 때
 @RestController
 public class UserRestController {
-    // public String htmlRendering() {
-    //     // ...
-    // }
-    //
-    // @ResponseBody
-    // public User json() {
-    //
-    // }
+    private final UserRepository userRepository;
+
+    public UserRestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getUser(@PathVariable("userId") String userId) {
+        return userRepository.getUser(userId);
+    }
 }
